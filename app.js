f4afeadc717bf90a0bf3c25871ba134b7a27ea28
Xurshid <xurshidjonxurshid45@gmail.com>
2023-01-28 15:43:55 +0300

    My git js

---
+++ app.js
@@ -0,0 +1,7 @@
+function oneTwo() {
+    let a = document.querySelector('.one-input').value;
+    document.querySelector('.one-h3').innerHTML = a;
+
+    let b = document.querySelector('.two-input').value;
+    document.querySelector('.two-h3').innerHTML = b;
+}
